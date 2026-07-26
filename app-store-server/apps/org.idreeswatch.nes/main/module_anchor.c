a1c5e329e92db010e5e15dfcc9eccfe6b783a2ac
mihkeymouse <malikidreeshasankhan@idrees.in>
2026-07-26 18:30:23 +0530

    Fix portable module link anchor

--- app-store-server/apps/org.idreeswatch.nes/main/module_anchor.c
+++ app-store-server/apps/org.idreeswatch.nes/main/module_anchor.c
@@ -3,9 +3,7 @@
  * official project_so helper compiles this tiny shim and links nes_core.a.
  */
 
-#include "idreeswatch_module.h"
-
-extern const idreeswatch_module_v1_t idreeswatch_module;
+extern const unsigned char idreeswatch_module;
 
 __attribute__((used))
 const void *idreeswatch_module_link_anchor = &idreeswatch_module;
